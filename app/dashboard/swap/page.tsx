e4874f0f0825a98ea9a4a89c4c28d9d8b10753e2
lucentpool <lucentpool2@gmail.com>
2023-12-12 21:46:05 +0100

    Add input fields to swap page

--- app/dashboard/swap/page.tsx
+++ app/dashboard/swap/page.tsx
@@ -1,15 +1,29 @@
-import { Card } from '@/app/ui/dashboard/cards';
-export default function Page() {
+import Swapinput from '@/app/ui/swap/swap-input';
+import Swapoutput from '@/app/ui/swap/swap-output';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { lusitana } from '@/app/ui/fonts';
+
+export default async function Page() {
   return (
-    <>
-      <p>Swap Page</p>
-      <div title="Swap Window">Swap Window</div>
-      <div>
-        <button>Approve</button>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Swap</h1>
       </div>
-      <div>
-        <button>Swap</button>
+      <div className="mt-4 flex-col items-center justify-between gap-2 md:mt-8">
+        <div>
+          <Swapinput placeholder="Amount in..." />
+        </div>
+        {/* <CreateInvoice /> */}
+        <div>
+          <Swapoutput placeholder="Amount out..." />
+        </div>
       </div>
-    </>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
   );
 }
