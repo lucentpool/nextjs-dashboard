22f2b11ab25fcb2919c6842d077223534cbe33fc
lucentpool <lucentpool2@gmail.com>
2023-12-12 19:35:45 +0100

    Add streaming

--- app/dashboard/swap/page.tsx
+++ app/dashboard/swap/page.tsx
@@ -1,3 +1,15 @@
+import { Card } from '@/app/ui/dashboard/cards';
 export default function Page() {
-  return <p>Swap Page</p>;
+  return (
+    <>
+      <p>Swap Page</p>
+      <div title="Swap Window">Swap Window</div>
+      <div>
+        <button>Approve</button>
+      </div>
+      <div>
+        <button>Swap</button>
+      </div>
+    </>
+  );
 }
